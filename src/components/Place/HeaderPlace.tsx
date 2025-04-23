e2b17d8fb9bcc064b9c95917f5e880adbd15e632
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2025-04-23 12:44:47 +0200

    add sticky return icon

--- src/components/Place/HeaderPlace.tsx
+++ src/components/Place/HeaderPlace.tsx
@@ -1,5 +1,5 @@
+import { Box, IconButton, Typography } from '@mui/material'
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn'
-import { Box, Typography, IconButton } from '@mui/material'
 import { useRouter } from 'next/navigation'
 import type { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime'
 
@@ -12,20 +12,28 @@ const HeaderPlace = ({ title }: HeaderPlacePros) => {
 
   return (
     <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '16px',
-      }}
+      sx={theme => ({
+        position: 'relative',
+        [theme.breakpoints.down('sm')]: {
+          paddingTop: '5%',
+        },
+      })}
     >
       <IconButton
         onClick={() => router.push('/')}
         aria-label="Retour"
-        sx={{
-          '&:hover': { color: 'primary.main' },
-        }}
+        sx={theme => ({
+          position: 'fixed',
+          top: '16px',
+          left: '16px',
+          zIndex: 1200,
+          backgroundColor: theme.palette.background.paper,
+          borderRadius: '50%',
+          boxShadow: 2,
+          '&:hover': {
+            color: 'primary.main',
+          },
+        })}
       >
         <KeyboardReturnIcon />
       </IconButton>
@@ -33,16 +41,17 @@ const HeaderPlace = ({ title }: HeaderPlacePros) => {
       <Typography
         variant="h6"
         component="div"
-        sx={{
+        sx={theme => ({
           textAlign: 'center',
-          flexGrow: 1,
           fontWeight: 600,
-        }}
+          marginBottom: '1%',
+          [theme.breakpoints.down('sm')]: {
+            marginBottom: '5%',
+          },
+        })}
       >
         {title}
       </Typography>
-
-      <Box sx={{ width: '48px' }} />
     </Box>
   )
 }
